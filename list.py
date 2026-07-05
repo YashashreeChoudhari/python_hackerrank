d108e6ee3b86340461faf6c8bedab0a285a0ae99
YashashreeChoudhari <yashashreechoudhari@gmail.com>
2026-07-05 22:29:48 +0530

    Adding list.py solution file
    Solved a HackerRank question on a list where you need to perform various operations on a list.

---
+++ list.py
@@ -0,0 +1,23 @@
+#Que. You are given a list of commands and you need to perform the corresponding operations on a list.
+
+if __name__ == '__main__':
+    N = int(input())
+    lst = []
+
+    for _ in range(N):
+        command = input().split()
+
+        if command[0] == "insert":
+            lst.insert(int(command[1]), int(command[2]))
+        elif command[0] == "print":
+            print(lst)
+        elif command[0] == "remove":
+            lst.remove(int(command[1]))
+        elif command[0] == "append":
+            lst.append(int(command[1]))
+        elif command[0] == "sort":
+            lst.sort()
+        elif command[0] == "pop":
+            lst.pop()
+        elif command[0] == "reverse":
+            lst.reverse()
