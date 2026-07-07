d9bf55c6983dfcfa193f9199b3fc0fd21b7284da
YashashreeChoudhari <yashashreechoudhari@gmail.com>
2026-07-07 22:47:51 +0530

    Added string_splitjoin.py
    Solution to Que. Split the string on " " (space) delimiter and join using a hyphen.

---
+++ string_splitjoin.py
@@ -0,0 +1,10 @@
+#Que. Split the string on a " " (space) delimiter and join using a - hyphen.
+def split_and_join(line):
+    line=line.split(" ")
+    line = "-".join(line)
+    return line
+     
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
