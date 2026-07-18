1264be195e2e80de743011116c6195ddf916d5bf
YashashreeChoudhari <yashashreechoudhari@gmail.com>
2026-07-18 22:28:50 +0530

    Added string_formated.py
    Solution of Que. Given an integer, print the following values for each integer from to: Decimal, Octal, Hexadecimal (capitalized), Binary

---
+++ string_formated.py
@@ -0,0 +1,18 @@
+#Que.Given an integer, , print the following values for each integer from to :
+# Decimal
+# Octal
+# Hexadecimal (capitalized)
+# Binary
+
+def print_formatted(number):
+    width = len(bin(number))-2
+    for i in range(1,number+1):
+        d = str(i).rjust(width)
+        o = oct(i)[2:].rjust(width)
+        x = hex(i)[2:].rjust(width).upper()
+        b = bin(i)[2:].rjust(width)
+        print(d, o, x, b)
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
