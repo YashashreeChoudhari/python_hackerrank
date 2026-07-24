7b1b746c7c35d99902c4c8bfdf5cbf6dbd5de6da
Yashashree Choudhari <yashashreechoudhari@gmail.com>
2026-07-24 23:52:54 +0530

    Added Question in merge_tools.py

--- merge_tools.py
+++ merge_tools.py
@@ -1,3 +1,8 @@
+#Que.Given a string s and an integer k (where len(s) is a multiple of k):
+# Split s into equal substrings of length k.
+# For each substring, remove duplicate characters while keeping only their first occurrence and preserving the original order.
+# Print each processed substring on a new line.
+
 def merge_the_tools(string, k):
     for i in range(0, len(string), k):
         substring = string[i:i + k]
